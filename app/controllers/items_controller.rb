ea9aa4a771537e0ae3b2c280c10e3b988fe6ada6
toru0209 <76867451+toru0209@users.noreply.github.com>
2021-02-05 15:34:56 +0900

    フロント/ユーザー管理機能実装

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,20 @@
+class ItemsController < ApplicationController
+  def index
+  end
+
+  def create
+    # @item = Item.new(item_params)
+    # if @item.save
+    #   redirect_to root_path
+    # else
+    #   render :new
+    # end
+  end
+
+  def new
+    # @item = item.new
+  end
+
+  def destroy
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/ }
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/ }
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :birthday
+  end
+
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+
+  has_many :items
+  has_many :orders
+end
