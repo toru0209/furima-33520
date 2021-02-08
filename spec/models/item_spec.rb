9d4609e5501d629dd9a91d76b492176ceb10ebbb
toru0209 <76867451+toru0209@users.noreply.github.com>
2021-02-08 14:22:19 +0900

    出品機能の修正3

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -97,62 +97,6 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include('Shipping day must be other than 1')
       end
-    
-
-      it '商品画像を1枚つけることが必須であること' do
-        @item.image = nil
-        @item.valid?
-        expect(@item.errors.full_messages).to include("Image can't be blank")
-      end
-
-      it '商品名が必須であること' do
-        @item.name = nil
-        @item.image = nil
-        @item.valid?
-        expect(@item.errors.full_messages).to include("Name can't be blank")
-      end
-
-      it '商品の説明が必須であること' do
-        @item.description = nil
-        @item.valid?
-        expect(@item.errors.full_messages).to include("Description can't be blank")
-      end
-
-      it 'カテゴリーの情報が必須であること' do
-        @item.category_id = nil
-        @item.valid?
-        expect(@item.errors.full_messages).to include("Category can't be blank")
-      end
-
-      it '商品の状態についての情報が必須であること' do
-        @item.condition_id = nil
-        @item.valid?
-        expect(@item.errors.full_messages).to include("Condition can't be blank")
-      end
-
-      it '配送料の負担についての情報が必須であること' do
-        @item.shipping_cost_id = nil
-        @item.valid?
-        expect(@item.errors.full_messages).to include("Shipping cost can't be blank")
-      end
-
-      it '発送元の地域についての情報が必須であること' do
-        @item.shipping_place_id = nil
-        @item.valid?
-        expect(@item.errors.full_messages).to include("Shipping place can't be blank")
-      end
-
-      it '発送までの日数についての情報が必須であること' do
-        @item.shipping_day_id = nil
-        @item.valid?
-        expect(@item.errors.full_messages).to include("Shipping day can't be blank")
-      end
-
-      it '価格についての情報が必須であること' do
-        @item.price = nil
-        @item.valid?
-        expect(@item.errors.full_messages).to include("Price can't be blank")
-      end
       
       it '299円以下では登録できないこと' do
           @item.price = 299
