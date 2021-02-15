d97225d61dae166ff902bf2e4d680de2beecbfae
toru0209 <76867451+toru0209@users.noreply.github.com>
2021-02-15 19:38:07 +0900

    レビュー後の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -51,7 +51,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    redirect_to action: :index unless current_user.id == @item.user.id
+    redirect_to action: :index unless current_user.id == @item.user.id && @item.order == nil
   end
 
   def set_item
